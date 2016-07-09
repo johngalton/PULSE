d5987fcb446ae6b5b1a8f8c522f4ad106abe44ac
John <ja9g10@gmail.com>
2016-07-09 08:56:04 +0100

    Added structs for the commands.

--- Firmware/ledStripController/inc/uart.h
+++ Firmware/ledStripController/inc/uart.h
@@ -10,6 +10,26 @@
 
 #include "stm32l0xx_hal.h"
 
+typedef struct
+{
+	uint8_t type;
+	uint8_t led1;
+	uint8_t count1;
+	uint8_t led2;
+	uint8_t count2;
+	uint8_t led3;
+	uint8_t count3;
+	uint8_t checksum;
+} uart_quick_function;
+
+typedef struct
+{
+	uint8_t address;
+	uint8_t command;
+	uint8_t length;
+	uint8_t *data;
+} uart_std_function;
+
 void uart_init(void);
 void uart_handle(void);
 
